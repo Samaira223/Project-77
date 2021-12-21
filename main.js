ca30c1786203ca25c2cbb8dffd347cf088da25a2
Samaira223 <84765610+Samaira223@users.noreply.github.com>
2021-12-21 19:43:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+console.log("My first console project");
+var first_name="Ranveer";
+var last_name="Singh";
+
+var ful_name=first_name.concat(last_name);
+console.log(ful_name);
+
+var a=10;
+var b=5;
+var add=a+b;
+console.log(add);
+
+var sub=a-b;
+console.log(sub);
+
+var mul=a*b;
+console.log(mul);
+
+var div=a/b;
+console.log(div);
+
+
+
